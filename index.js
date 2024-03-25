4f314ed6cbeced2e977655b81fe745e057c9051e
root <root@SURFACE.>
2024-03-25 20:04:39 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const drivers=[];
+
+function findMatching(drivers,query){
+    return drivers.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
+console.log(findMatching(drivers,"Bobby"));
+
+function fuzzyMatch(drivers,query){
+   // drivers.filter(drivers.startsWith("Sa"))
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+console.log(fuzzyMatch);
+
+function matchName(drivers,query){
+    return drivers.filter(driver =>driver.name.toLowerCase() === query.toLowerCase());
+}
+console.log(matchName(drivers,"Bobby"));
